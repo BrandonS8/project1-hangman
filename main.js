a9c0beadcea026dc163497aae0613deb06d2d047
Brandon Sears <brandonsears8@gmail.com>
2018-02-10 21:05:09 -0500

    added more functions to make answer hide and show lines for characters

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-//variables for elements
+// variables for elements
 const wordBox = document.querySelector('.wordBox')
 const startGameButton = document.querySelector('.startGame')
-const answerHeading = document.querySelector('.answerHeading')
 const letterHolder = document.querySelector('.letters')
-//alphabet array
-const alphabet = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+const answerHolder = document.querySelector('.answerHeading')
+// alphabet array
+const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 // variable to hold answer
 let theAnswer = ''
-//generate array of letters and add click events to them
+let theAnswerArray = []
+// generate array of letters and add click events to them
 
-alphabet.forEach(function(letter){
+alphabet.forEach(function (letter) {
   let div = document.createElement('div')
   div.textContent = letter
   div.classList.add('letter')
@@ -18,30 +19,58 @@ alphabet.forEach(function(letter){
   letterHolder.appendChild(div)
 })
 
-//start game by hiding answer
+// start game by hiding answer
 startGameButton.addEventListener('click', start)
 
-function start(){
-//RegExp for checking if numbers are inputted?
-    if(wordBox.value !== ''){
-        theAnswer = wordBox.value.toUpperCase()
-        answerHeading.innerHTML = wordBox.value.replace(/[a-zA-Z]/g, "_")
-        // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/replace
-        wordBox.style.display = 'none'
-        startGameButton.style.display = 'none'
-        console.log(theAnswer)
+function start () {
+  if (wordBox.value !== '') {
+      theAnswer = wordBox.value.toUpperCase()
+      theAnswerArray = wordBox.value.toUpperCase().split('')
+      hideAnswer()
+      wordBox.style.display = 'none'
+      startGameButton.style.display = 'none'
+      console.log(theAnswer)
+      console.log(theAnswerArray)
     } else {
-        alert('Please enter a word')
+      alert('Please enter a word')
     }
 }
 
-function pressedLetter(){
-    console.log(this.textContent)
+function pressedLetter () {
+  this.textContent
 }
 
-wordBox.addEventListener('keypress', function(evt) { 
-    if (evt.keyCode === 13) {
-        evt.preventDefault()
-        start()
+wordBox.addEventListener('keypress', function (evt) {
+  if (evt.keyCode === 13) {
+      evt.preventDefault()
+      start()
     }
-  })
+})
+function hideAnswer () {
+  theAnswerArray.forEach(function (letter) {
+      if (letter != ' '){
+        let div = document.createElement('div')
+        div.textContent = letter
+        div.classList.add('hiddenAnswer')
+        div.addEventListener('click', showAnswerLetter)
+        answerHolder.appendChild(div)
+      } else{
+        let div = document.createElement('div')
+        div.textContent = letter
+        div.classList.add('hiddenAnswer')
+        div.style.borderBottom =  'none'
+        answerHolder.appendChild(div)
+      }
+    })
+}
+
+function showAnswerLetter() {
+    this.style.color = 'black'
+  }
+
+
+
+
+
+
+//   https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_split
