d52e513a5b8f88ae2c9d5c710ad23b174914e986
Brandon Sears <brandonsears8@gmail.com>
2018-02-10 21:57:44 -0500

    win/loss logic works with a minor error

--- main.js
+++ main.js
@@ -3,12 +3,18 @@ const wordBox = document.querySelector('.wordBox')
 const startGameButton = document.querySelector('.startGame')
 const letterHolder = document.querySelector('.letters')
 const answerHolder = document.querySelector('.answerHeading')
+let chars = []
 // alphabet array
 const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 // variable to hold answer
 let theAnswer = ''
 let theAnswerArray = []
+// wrong counter
+let wrongLetters = 0
+//right counter
+let rightLetters = 0
+let winNumber = 0
 // generate array of letters and add click events to them
 
 alphabet.forEach(function (letter) {
@@ -26,7 +32,7 @@ function start () {
   if (wordBox.value !== '') {
     theAnswer = wordBox.value.toUpperCase()
     theAnswerArray = wordBox.value.toUpperCase().split('')
-    hideAnswer()
+    hideAnswer(setChars)
     wordBox.style.display = 'none'
     startGameButton.style.display = 'none'
     console.log(theAnswer)
@@ -35,44 +41,71 @@ function start () {
     alert('Please enter a word')
   }
 }
-
+let wrongCount = 0
 function pressedLetter () {
-  let pressed = this.textContent
-  let chars = document.querySelectorAll('.hiddenAnswer')
+  let pressedContent = this.textContent
+  let pressed = this
+  let i = 0
   chars.forEach(function (letter) {
-    console.log(letter)
-    if (letter.innerHTML === pressed) {
-        letter.style.color = 'black'
-      }
+    if (letter.innerHTML === pressedContent) {
+      i++
+      rightLetters++
+      checkWin()
+      letter.style.color = 'black'
+      pressed.style.color = 'green'
+    } else if (i <= 0) {
+      wrongCount++
+      wrongAnswer(pressed)
+    }
   })
 }
 
+function wrongAnswer (pressed) {
+  if (wrongCount === chars.length) {
+    wrongLetters += 1
+    pressed.style.color = 'red'
+    console.log('Wrong!' + wrongLetters)
+    wrongCount = 0
+  }
+}
+// NOTE TO ME: last letter being right throws a wrong! fix!
 wordBox.addEventListener('keypress', function (evt) {
   if (evt.keyCode === 13) {
     evt.preventDefault()
     start()
   }
 })
-function hideAnswer () {
+function hideAnswer (callback) {
   theAnswerArray.forEach(function (letter) {
     if (letter != ' ') {
-        let div = document.createElement('div')
-        div.textContent = letter
-        div.classList.add('hiddenAnswer')
-        div.addEventListener('click', showAnswerLetter)
-        answerHolder.appendChild(div)
-      } else {
-        let div = document.createElement('div')
-        div.textContent = letter
-        div.classList.add('hiddenAnswer')
-        div.style.borderBottom = 'none'
-        answerHolder.appendChild(div)
-      }
+      let div = document.createElement('div')
+      div.textContent = letter
+      div.classList.add('hiddenAnswer')
+      div.addEventListener('click', showAnswerLetter)
+      answerHolder.appendChild(div)
+      winNumber++
+    } else {
+      let div = document.createElement('div')
+      div.textContent = letter
+      div.classList.add('hiddenAnswer')
+      div.style.borderBottom = 'none'
+      answerHolder.appendChild(div)
+    }
+    callback()
   })
+}
+
+function setChars () {
+  chars = document.querySelectorAll('.hiddenAnswer')
 }
 
 function showAnswerLetter () {
   this.style.color = 'black'
 }
 
+function checkWin(){
+    if(rightLetters === winNumber){
+        console.log('winner!')
+    }
+}
 //   https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_split
