cf6c1950a49fbd2d9a42300db1410a977e17f110
Brandon Sears <brandonsears8@gmail.com>
2018-02-14 10:36:49 -0500

    added directions and minor fixes

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ const letterHolder = document.querySelector('.letters')
 const answerHolder = document.querySelector('.answerHeading')
 let chars = []
 const man = document.querySelector('.man').children
-// alphabet array
+// alphabet array for the letter creator
 const alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 // theme array
 themeArray = ['supreme.css', 'light.css', 'greyscale.css']
@@ -18,24 +18,13 @@ let currentWord = 0
 let wrongLetters = 0
 // right counter
 let rightLetters = 0
+// letters needed to win
 let winNumber = 0
 let score = 0
 // current player
 let currentPlayer = 'player1'
 
-// generate array of letters and add click events to them
-function createLetters () {
-  alphabet.forEach(function (letter) {
-    let div = document.createElement('div')
-    div.textContent = letter
-    div.classList.add('letter')
-    div.addEventListener('click', pressedLetter)
-    letterHolder.appendChild(div)
-  })
-}
-
-console.log(sessionStorage.getItem('loadedBefore'))
-
+// checking if page loaded before to set defaults for scoring
 if (sessionStorage.getItem('loadedBefore') === null) {
   currentPlayer = 'player1'
   score = 0
@@ -50,62 +39,56 @@ if (sessionStorage.getItem('loadedBefore') === null) {
   updatePlayer()
   updateScore()
 }
-console.log(sessionStorage.getItem(`${currentPlayer}score`))
-console.log(sessionStorage.getItem('loadedBefore'))
-console.log(sessionStorage.getItem('lastPlayer'))
-
-
-// scores
+// scoring
 function updateScore () {
-    console.log(score)
-    console.log(currentPlayer)
+  console.log(score)
+  console.log(currentPlayer)
   if (currentPlayer === 'player1') {
-      sessionStorage.setItem('player1score', score)
-      document.querySelector('.player1score').innerHTML = `Player 1: ${score}`
-    } else {
-      sessionStorage.setItem('player2score', score)
-    }
+    sessionStorage.setItem('player1score', score)
+    document.querySelector('.player1score').innerHTML = `Player 1: ${score}`
+  } else {
+    sessionStorage.setItem('player2score', score)
+  }
 
-   let savedScore1 =   sessionStorage.getItem('player1score')
-   let savedScore2 = sessionStorage.getItem('player2score')
-    document.querySelector('.player1score').innerHTML = `Player 1: ${savedScore1}`
-    document.querySelector('.player2score').innerHTML = `Player 2: ${savedScore2}`
+  let savedScore1 = sessionStorage.getItem('player1score')
+  let savedScore2 = sessionStorage.getItem('player2score')
+  document.querySelector('.player1score').innerHTML = `Player 1: ${savedScore1}`
+  document.querySelector('.player2score').innerHTML = `Player 2: ${savedScore2}`
 }
 
 document.querySelector('.clearScores').addEventListener('click', function () {
-    score = 0
-    sessionStorage.setItem('player1score', 0)
-    sessionStorage.setItem('player2score', 0)
-    sessionStorage.setItem('lastPlayer', 'player2')
-    sessionStorage.removeItem('loadedBefore')
-    changeLastPlayer()
-    updateScore()
+  score = 0
+  sessionStorage.setItem('player1score', 0)
+  sessionStorage.setItem('player2score', 0)
+  sessionStorage.setItem('lastPlayer', 'player2')
+  sessionStorage.removeItem('loadedBefore')
+  changeLastPlayer()
+  updateScore()
 })
-// currentplayer
 
+// currentplayer
 function changeLastPlayer () {
   let lastPlayer = sessionStorage.getItem('lastPlayer')
   if (lastPlayer === 'player2') {
-  currentPlayer = 'player1'
-  sessionStorage.setItem('lastPlayer', 'player1')
-} else if (lastPlayer === 'player1') {
-  currentPlayer = 'player2'
-  sessionStorage.setItem('lastPlayer', 'player2')
-}
-score = sessionStorage.getItem(`${currentPlayer}score`)
+    currentPlayer = 'player1'
+    sessionStorage.setItem('lastPlayer', 'player1')
+  } else if (lastPlayer === 'player1') {
+    currentPlayer = 'player2'
+    sessionStorage.setItem('lastPlayer', 'player2')
+  }
+  score = sessionStorage.getItem(`${currentPlayer}score`)
   updatePlayer()
 }
 console.log(currentPlayer)
 function updatePlayer () {
   if (currentPlayer === 'player1') {
-      document.querySelector('.turnTeller').innerHTML = `Player 1's Guess`
-    } else {
-      document.querySelector('.turnTeller').innerHTML = `Player 2's Guess`
-    }
+    document.querySelector('.turnTeller').innerHTML = `Player 1's Guess`
+  } else {
+    document.querySelector('.turnTeller').innerHTML = `Player 2's Guess`
+  }
 }
 
-// theme changer
-
+// theme changer, saves on reload as well
 let currentTheme = 1
 if (sessionStorage.getItem('savedTheme')) {
   currentTheme = sessionStorage.getItem('savedTheme')
@@ -116,16 +99,52 @@ document.querySelector('#theme').setAttribute('href', themeArray[currentTheme])
 document.querySelector('.themeButton').addEventListener('click', changeTheme)
 function changeTheme () {
   if (currentTheme < (themeArray.length - 1)) {
-      currentTheme++
-      sessionStorage.setItem('savedTheme', currentTheme)
-    } else {
-      currentTheme = 0
-      sessionStorage.setItem('savedTheme', currentTheme)
-    }
+    currentTheme++
+    sessionStorage.setItem('savedTheme', currentTheme)
+  } else {
+    currentTheme = 0
+    sessionStorage.setItem('savedTheme', currentTheme)
+  }
   document.querySelector('#theme').setAttribute('href', themeArray[currentTheme])
+}
+// show directions
+document.querySelector('.directions').style.display = 'none' //the click if statement doesnt work the first click without this 
+document.querySelector('.showDirections').addEventListener('click', function () {
+  let d = document.querySelector('.directions')
+  let b = document.querySelector('.showDirections')
+    if(d.style.display === 'none'){
+        d.style.display = 'initial'
+        wordBox.style.display = 'none'
+        startGameButton.style.display = 'none'
+        b.innerHTML = 'Hide Directions'
+    } else {
+        d.style.display = 'none'
+        wordBox.style.display = 'initial'
+        startGameButton.style.display = 'initial'
+        b.innerHTML = 'View Directions'
+    }
+})
+
+// generate array of letters and add click events to them when start calls it
+function createLetters () {
+  alphabet.forEach(function (letter) {
+    let div = document.createElement('div')
+    div.textContent = letter
+    div.classList.add('letter')
+    div.addEventListener('click', pressedLetter)
+    letterHolder.appendChild(div)
+  })
 }
 
 // start game by hiding answer
+// enter key to start from wordbox or use start button
+wordBox.addEventListener('keypress', function (evt) {
+  if (evt.keyCode === 13) {
+    evt.preventDefault()
+    start()
+  }
+})
+
 startGameButton.addEventListener('click', start)
 
 function start () {
@@ -139,11 +158,12 @@ function start () {
     createLetters()
     wordBox.style.display = 'none'
     startGameButton.style.display = 'none'
+    document.querySelector('.showDirections').style.display = 'none'
   } else {
     alert('Please enter a word')
   }
 }
-
+// puts the letters into words so they won't get flex-wrapped in a weird way seperated from parts of the word
 function setWords () {
   theAnswerArrayWords.forEach(function (word) {
     let div = document.createElement('div')
@@ -151,7 +171,7 @@ function setWords () {
     answerHolder.appendChild(div)
   })
 }
-
+// guessing
 let wrongCount = 0
 function pressedLetter () {
   let pressedContent = this.textContent
@@ -183,13 +203,7 @@ function wrongAnswer (pressed) {
     checkLoss()
   }
 }
-
-wordBox.addEventListener('keypress', function (evt) {
-  if (evt.keyCode === 13) {
-    evt.preventDefault()
-    start()
-  }
-})
+// hide answers if they are letters, display all the other characters
 function hideAnswer (callback) {
   let word = document.querySelectorAll('.hiddenAnswerWord')
   let normal = /[a-zA-Z]/
@@ -231,49 +245,41 @@ function checkWin () {
 }
 function checkLoss () {
   if (wrongLetters >= 7) {
-      console.log('You lost')
-      winLose('lose')
-    }
+    console.log('You lost')
+    winLose('lose')
+  }
 }
 
+// adds to the hangman
 function makeMan (value) {
   man[value].style.opacity = 1
 }
-
+// displays if user won or lost
 function winLose (value) {
   letterHolder.style.display = 'none'
   document.querySelector('.answer').style.flexDirection = 'column'
   document.querySelector('.right').style.display = 'none'
   document.querySelector('.winloss').style.display = 'inline'
   if (value === 'win') {
-      score++
-      document.querySelector('.winloss').classList.add('won')
-      document.querySelector('.winloss').textContent = `YOU WIN!`
-      showAnswer()
-      updateScore()
-    } else if (value === 'lose') {
-      document.querySelector('.winloss').classList.add('lost')
-      document.querySelector('.winloss').textContent = `YOU LOSE!`
-      showAnswer()
-    }
+    score++
+    document.querySelector('.winloss').classList.add('won')
+    document.querySelector('.winloss').textContent = `YOU WIN!`
+    showAnswer()
+    updateScore()
+  } else if (value === 'lose') {
+    document.querySelector('.winloss').classList.add('lost')
+    document.querySelector('.winloss').textContent = `YOU LOSE!`
+    showAnswer()
+  }
 }
-
+// shows the answer at end of the game
 function showAnswer () {
   document.querySelectorAll('.hiddenAnswer').forEach(function (ans) {
-      ans.classList.add('unhiddenAnswer')
-      ans.classList.remove('hiddenAnswer')
-    })
+    ans.classList.add('unhiddenAnswer')
+    ans.classList.remove('hiddenAnswer')
+  })
 }
 
 // https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_split
 // https://stackoverflow.com/questions/13946651/matching-special-characters-and-letters-in-regex
 // note to self: look more into regexp is may be more useful https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
-
-// later on split this into mulitple objects
-// like an object for the answer stuff
-// object for the letters
-// object for the hangman
-// add changing background for translucent theme
-
-
-//css still needs fixed for smaller screens
